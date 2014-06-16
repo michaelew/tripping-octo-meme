d1311d4b9bb613019230922f8d8598895bea2aaa
thezilla <321homerepair@gmail.com>
2014-06-16 19:27:39 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,23 +2,30 @@
 
 import sys
 
-balance = sys.argv[0]
-annualInterestRate = sys.argv[1]
-monthlyPaymentRate = sys.argv[2]
+balance = 4213
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+
+months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+monthlyInterestRate = (annualInterestRate) / 2.0
+minimumMonthlyPayment = (monthlyPaymentRate) * (balance)
+monthlyUnpaidBalance = (balance) - (minimumMonthlyPayment)
+
 
 def pay_minimum(balance, annualInterestRate, monthlyPaymentRate):
-    months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-    monthlyInterestRate = (annualInterestRate) / 12.0
-    minimumMonthlyPayment = (monthlyPaymentRate) * (balance)
-    monthlyUnpaidBalance = (balance) - (minimumMonthlyPayment)
     updatedBalanceEachMonth = (monthlyUnpaidBalance) + (monthlyInterestRate * monthlyUnpaidBalance)
-
+    minimumMonthlyPayment = (monthlyPaymentRate) * (updatedBalanceEachMonth)
+    totalPaymentsMade = 0
     for month in months:
+        balance = round(updatedBalanceEachMonth, 2)
+        updatedBalanceEachMonth -= minimumMonthlyPayment
+        minimumMonthlyPayment = (monthlyPaymentRate) * (updatedBalanceEachMonth)
         print "Month: " + month
         print "Minimum monthly payment: " + str(round(minimumMonthlyPayment, 2))
         print "Remaining balance: " + str(round(updatedBalanceEachMonth, 2))
+        totalPaymentsMade += minimumMonthlyPayment
 
-    print "Total paid: " + balance
+    print "Total paid: " + str(round(totalPaymentsMade, 2))
     print "Remaining balance: " + str(round(updatedBalanceEachMonth, 2))
 
 pay_minimum(balance, annualInterestRate, monthlyPaymentRate)
